9f366ff7d631bedc3f23f61d4e7d32ddb3b0b7c7
manojLovesCoding <mkum9417@gmail.com>
2026-04-14 20:20:48 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -19,8 +19,8 @@ app.get("/", (req, res) => {
   res.send("api is working fine");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+//const PORT = process.env.PORT || 3000;
+//app.listen(PORT, () => {
+//  console.log(`Server is running on port ${PORT}`);
+//});
 export default app;
